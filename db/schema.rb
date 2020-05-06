00ebed9b06e2a68abc37f5c65c484efe156e0793
Joe Reiff <jcreiff@gmail.com>
2020-05-06 17:46:26 -0400

    create seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_205900) do
+ActiveRecord::Schema.define(version: 2020_05_06_211059) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+artists = []
+15.times do
+  a = Artist.create!(name: Faker::Music::RockBand.name)
+  artists << a
+end
+
+statuses = ["Future", "In Progress", "Learned", "Memorized"]
+keys = ["A", "B", "C", "D", "E", "F", "G"]
+
+50.times do
+  Song.create!(title: Faker::App.name, artist_id: artists.sample.id,
+               status: statuses.sample, key: keys.sample, high_note: keys.sample,
+               last_performed: Faker::Date.between(from: 1.year.ago, to: Date.today),
+               capo: rand(1..6), lyrics: Faker::Lorem.paragraphs,
+               video: Faker::LoremFlickr.image)
+end
