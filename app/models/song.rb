2ada9f21578fd96774fa219abea056c5c07c4f21
Joe Reiff <jcreiff@gmail.com>
2020-05-12 15:30:06 -0400

    move youtube embed logic to model

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,8 @@ class Song < ApplicationRecord
   scope :by_title, -> { order(:title) }
   scope :by_date, -> { order(:last_performed) }
   scope :by_status, -> { order(:status) }
+
+  def embedded_video
+    video.sub("watch?v=", "embed/")
+  end
 end
